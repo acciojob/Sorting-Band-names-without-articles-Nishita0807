ee72d7812a00505b061f4db1e223333e99581ce9
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-08 15:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here
+// Define the array of band names
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+// Function to remove articles and return a cleaned name
+function removeArticles(name) {
+    return name.replace(/^(a|an|the)\s+/i, '');
+}
+
+// Remove articles and sort the array
+touristSpots = touristSpots.map(removeArticles).sort();
+
+// Function to generate the HTML list
+function generateBandList() {
+    const ul = document.getElementById('band');
+    
+    // Clear any existing list items
+    ul.innerHTML = '';
+    
+    // Create and append list items
+    touristSpots.forEach(function (spot) {
+        const li = document.createElement('li');
+        li.textContent = spot;
+        ul.appendChild(li);
+    });
+}
+
+// Call the function to generate the HTML list
+generateBandList();
 
